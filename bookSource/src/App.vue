dd951c6ccbc2aa0ce02c4499284ea9e224be0628
lichong <wslcwslc@126.com>
2024-04-11 18:00:36 +0800

    feat: 去重页面

--- bookSource/src/App.vue
+++ bookSource/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="body-container">
-    <Greet />
+    <RepeatIndex />
   </div>
 </template>
 
 <script setup lang="ts">
-import Greet from "./components/Greet.vue";
+import RepeatIndex from "@/views/repeat/index.vue";
 </script>
 
 <style scoped lang="scss">
---
+++ bookSource/src/views/repeat/index.vue
@@ -0,0 +1,5 @@
+<template>11</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
