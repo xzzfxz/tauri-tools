1afa6dcf05fd4d31d5b7fd0d68a7c5badfc33de3
lichong <wslcwslc@126.com>
2024-04-11 17:21:19 +0800

    feat: 书源管理

---
+++ bookSource/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="body-container">
+    <Greet />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Greet from "./components/Greet.vue";
+</script>
+
+<style scoped lang="scss">
+.body-container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
