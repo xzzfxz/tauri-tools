55fc537abe0a9aba9e12b32f70968b78664c9c16
lichong <wslcwslc@126.com>
2024-09-20 17:46:48 +0800

    feat: 请求成功

--- fanqie/src-tauri/src/controller/mod.rs
+++ fanqie/src-tauri/src/controller/mod.rs
@@ -2,7 +2,14 @@ use crate::service;
 
 // 查询
 #[tauri::command]
-pub fn search(text: String) -> String {
-    let _ = service::search(&text);
+pub async fn search(text: String) -> String {
+    match service::search(&text).await {
+        Ok(_) => {
+            println!("成功");
+        }
+        Err(info) => {
+            println!("错误：{:#?}", info)
+        }
+    };
     return text;
 }
--- fanqie/src-tauri/src/service/mod.rs
+++ fanqie/src-tauri/src/service/mod.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use reqwest::header::USER_AGENT;
 use serde_json::Value;
 
 /**
@@ -10,18 +11,21 @@ pub async fn search(text: &str) -> Result<()> {
     println!("search: {}", text);
     let url = format!(
         "https://fanqienovel.com/api/author/search/search_book/v1?\
-        filter=127.127.127.127\
+        filter=127%2C127%2C127%2C127\
         &page_count=10\
         &page_index=0\
+        &query_type=0\
         &query_word={}\
-        &msToken=mUEYxyDzWRtBVovBxeK7k3hwOzaumwZ8Ko5zg4kCZHcRbhRbzwWNlK3Zhgx4dRpsWkpQAFnlhMbmWGFENpSgPKpNdGlNQo-\
+        &msToken=mUEYxyDzWRtBVovBxeK7k3hwOzaumwZ8Ko5zg4kCZHcRbhRbzwWNlK3Zhgx4dRpsWkpQAFnlhMbmWGFENpSgPKpNdGlNQo-rN_nK4aL3CvVuq22O885Q1x12vfszkYM%3D\
         &a_bogus=EJUmgO2IMsm1XE3adhkz9r4mIr60YW-LgZEzJpgELUwQ",
         text
     );
     let client = reqwest::Client::new();
-    let res: reqwest::Response = client.get(url).send().await?;
-    let list = res.json::<Vec<Value>>().await?;
-    println!("{:?}", list);
+    let res: reqwest::Response = client.get(url).header(USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36").send().await?;
+    if res.status().is_success() {
+        let text = res.text().await?;
+        println!("{:?}", text);
+    }
 
     // delete_fn(list, &mut book_map)?;
 
