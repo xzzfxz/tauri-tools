4fe2b89cb10089771ceebedd948df7fde09c3f5a
lichong <wslcwslc@126.com>
2024-09-20 16:15:17 +0800

    feat: 番茄

--- fanqie/src-tauri/src/service/mod.rs
+++ fanqie/src-tauri/src/service/mod.rs
@@ -1,18 +1,27 @@
 use anyhow::Result;
+use serde_json::Value;
 
 /**
  * @description: 查询
  * @param {*} text  关键字
  * @return {*}
  */
-pub fn search(text: &str) -> Result<()> {
+pub async fn search(text: &str) -> Result<()> {
     println!("search: {}", text);
-    let url = format!("");
-
-    // let client = reqwest::Client::new();
-    // let res: reqwest::Response = client.get(url).send().await?;
-    // let list = res.json::<Vec<Value>>().await?;
-    // let pre_len = list.len();
+    let url = format!(
+        "https://fanqienovel.com/api/author/search/search_book/v1?\
+        filter=127.127.127.127\
+        &page_count=10\
+        &page_index=0\
+        &query_word={}\
+        &msToken=mUEYxyDzWRtBVovBxeK7k3hwOzaumwZ8Ko5zg4kCZHcRbhRbzwWNlK3Zhgx4dRpsWkpQAFnlhMbmWGFENpSgPKpNdGlNQo-\
+        &a_bogus=EJUmgO2IMsm1XE3adhkz9r4mIr60YW-LgZEzJpgELUwQ",
+        text
+    );
+    let client = reqwest::Client::new();
+    let res: reqwest::Response = client.get(url).send().await?;
+    let list = res.json::<Vec<Value>>().await?;
+    println!("{:?}", list);
 
     // delete_fn(list, &mut book_map)?;
 
