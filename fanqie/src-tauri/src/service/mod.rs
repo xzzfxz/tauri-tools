ad7e33225607f4349915575ad124f79cd41a5474
lichong <wslcwslc@126.com>
2024-09-20 15:48:27 +0800

    feat: 番茄

--- fanqie/src-tauri/src/service/mod.rs
+++ fanqie/src-tauri/src/service/mod.rs
@@ -7,5 +7,19 @@ use anyhow::Result;
  */
 pub fn search(text: &str) -> Result<()> {
     println!("search: {}", text);
+    let url = format!("");
+
+    // let client = reqwest::Client::new();
+    // let res: reqwest::Response = client.get(url).send().await?;
+    // let list = res.json::<Vec<Value>>().await?;
+    // let pre_len = list.len();
+
+    // delete_fn(list, &mut book_map)?;
+
+    // let mut last_list: Vec<Value> = book_map.into_values().collect();
+    // let cur_len = last_list.len();
+    // REPEAT_LIST.lock().unwrap().clear();
+    // REPEAT_LIST.lock().unwrap().append(&mut last_list);
+    // Ok(RepeatRes { pre_len, cur_len })
     Ok(())
 }
